be94c13afcbfa3bdbea6658bed647d4dae7f81bb
tunt01102 <32449262+tunt01102@users.noreply.github.com>
2024-10-27 04:53:34 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from mightyjaxx.tests.test_home import *  # Import everything from import_all.py
+# Run the test
+if __name__ == "__main__":
+    #run test case 001
+    asyncio.run(testcase001_check_about_us_page())
